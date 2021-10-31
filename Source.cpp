267803b3a3585d8f15b6350c0ab9a6f4e377f5d2
Destice <r.chron@wp.pl>
2021-10-31 17:43:26 +0100

    Implemented matrix addition, dimension checking, exception handling

--- Source.cpp
+++ Source.cpp
@@ -21,9 +21,9 @@ public:
 			v[i].resize(width); //rozrzeszam kazdy wiersz do podanej wartosci
 		}
 	}
-	void insert_value(int row, int column, int value) //brak implementacji
+	void insert_value(int row, int column, int value) 
 	{
-		//v[row][column] = value;
+		v[row][column] = value;
 	}
 	void auto_insert_values()
 	{
@@ -58,22 +58,52 @@ public:
 	{
 		return width;
 	}
+	Matrix matrix_addition(Matrix added)
+	{
+		try {
+
+		
+			if (added.return_height() == height && added.return_width() == width)
+			{
+				Matrix* m3 = new Matrix(height, width);
+				for (int i = 0; i < height; i++)
+				{
+					for (int j = 0; j < width; j++)
+					{
+						m3->insert_value(i, j, v[i][j] + added.return_value(i, j));
+					}
+
+				}
+				return *m3;
+			}
+			else
+				cout << "Dodawanie niemozliwe - wymiary macierzy niezgodne";
+		}
+		catch (exception& e)
+		{
+			cerr << e.what();
+		}
+	}
 
 };
 
 
 int main()
 {
-	Matrix m1(5,3);
+	Matrix m1(5,3); //macierz(wiersze,kolumny)
 	m1.auto_insert_values();
 	m1.print_matrix();
 
 	cout << endl;
 
-	Matrix m2(5, 3);
+	Matrix m2(4, 2);
 	m2.auto_insert_values();
 	m2.print_matrix();
 
+	cout << endl;
+
+	Matrix m3 = m1.matrix_addition(m2);
+	m3.print_matrix();
 
 	return 0;
 }
